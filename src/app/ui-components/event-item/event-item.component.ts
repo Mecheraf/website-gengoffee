d9ddd1f716d9f1b34127be0d5de1c761f4da8340
Salim Baroudi <salim@macbook-pro-de-salim.home>
2022-08-09 18:44:47 +0200

    WIP, event cards almost done, need to handle date parameters

--- src/app/ui-components/event-item/event-item.component.ts
+++ src/app/ui-components/event-item/event-item.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+const localeToFlag: any = {
+  'jp' : '🇯🇵',
+  'en' : '🇬🇧',
+  'es' : '🇪🇸'
+}
 
 @Component({
   selector: 'app-event-item',
@@ -7,9 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventItemComponent implements OnInit {
 
+  private defaultLanguage: string = '🇫🇷';
+  public flags: string = '';
+  public hour: string = '';
+
+  @Input() language: string = "jp"; // Use locale e.g. fr, en, jp
+  @Input() active: boolean = false;
+  @Input() date: string = "";
+  @Input() place: string = "";
   constructor() { }
 
   ngOnInit(): void {
+    this.createFlags();
+  }
+
+  private createFlags(): void {
+    this.flags = this.defaultLanguage + localeToFlag[this.language];
   }
 
 }
