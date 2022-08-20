fa35bcd156b2f1101c502febec427611cca17f6c
Salim Baroudi <salim@macbook-pro-de-salim.home>
2022-08-20 22:16:30 +0200

    Header home and footer done with responsive, need some adjustements

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public showMobileMenu: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openMenu() {
+    this.showMobileMenu = true;
+  }
+
+  closeMenu() {
+    this.showMobileMenu = false;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  isMobile() {
+    return window.innerWidth < 768;
+  }
 }
