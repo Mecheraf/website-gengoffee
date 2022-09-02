142375670b99ed89961814aecac30f5f587d68af
alan <mecheraf@gmail.com>
2022-09-02 11:35:12 +0200

    pull event service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../services/event.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public nextEvents:any;
+  public pastEvents:any;
+  constructor(private eventservice: EventService) { }
 
   ngOnInit(): void {
   }
+
+  getNextEvents(limit:number) {
+    this.eventservice.getNextEvents({params:{limit: limit}}).subscribe((data) => {
+      this.nextEvents = data;
+    })
+  }
+/*
+  getPastEvents(limit:number) {
+    this.eventservice.getPastEvents({params:{limit: limit}}).subscribe((data) => {
+      this.nextEvents = data;
+    })
+  }*/
 
 }
