c598e6f0892686ea2584b70734d3965d07500122
alan <mecheraf@gmail.com>
2023-10-25 18:36:14 +0200

    done with the list

--- src/app/registered/registered.component.ts
+++ src/app/registered/registered.component.ts
@@ -1,15 +1,18 @@
-import { ListKeyManager } from '@angular/cdk/a11y';
 import { Component, OnInit } from '@angular/core';
 import { __values } from 'tslib';
 import { RegisteredService } from '../services/registered.service';
 import { EventService } from '../services/event.service';
+import { switchMap, tap } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+
 
 
 interface registeredUser {
-  firstName:string,
-  lastName:string,
-  language:string[],
-  diet:string[]
+  name:string,
+  languages:string[],
+  mail:string,
+  diet:string[], 
+  id_event:number
 }
 
 interface eventsAttendees {
@@ -29,101 +32,69 @@ interface eventsAttendees {
 export class RegisteredComponent implements OnInit {
 
 
-
   public today = new Date()
   public nextEvents:any;
   public registeredList: any = [];
   public events:eventsAttendees[] = [];
- 
-  public alanUser:registeredUser = {
-    firstName:"Alan",
-    lastName:"Mecheraf",
-    language:["3","3","1"],
-    diet:[]
-  }
-
-  public mayuUser:registeredUser = {
-    firstName:"Mayu",
-    lastName:"Koketsu",
-    language:["2","1","3"],
-    diet:["Vegetarian"]
-  }
-  
-  public salimUser:registeredUser = {
-    firstName:"Salim",
-    lastName:"Baroudi",
-    language:["3","2","1"],
-    diet:["Halal"]
-  }
-  
-  public steveUser:registeredUser = {
-    firstName:"Steve",
-    lastName:"Michiels",
-    language:["3","1","1"],
-    diet:["Gros noyau"]
-  }
-  
-  public brandonUser:registeredUser = {
-    firstName:"Brandon",
-    lastName:"Martins",
-    language:["3","2","1"],
-    diet:["Vegetarian"]
-  }
-
-  public registeredList1: registeredUser[] = [this.mayuUser, this.alanUser];
-  public registeredList2: registeredUser[] = [this.alanUser, this.salimUser, this.steveUser, this.brandonUser];
-
-
-
-  // public events:eventsAttendees[] = [
-  //   {id:51, date:new Date("2022-11-20"), registeredList:this.registeredList1}, 
-  //   {id:52, date: new Date("2022-11-27"), registeredList:this.registeredList2},
-  //   {id:53, date: new Date("2022-12-04"), registeredList:this.registeredList2},
-  //   {id:53, date: new Date("2022-12-04"), registeredList:this.registeredList2},
-  //   {id:53, date: new Date("2022-12-04"), registeredList:this.registeredList2},
-  //   {id:53, date: new Date("2022-12-04"), registeredList:this.registeredList2},
-  //   {id:53, date: new Date("2022-12-04"), registeredList:this.registeredList2},
-  //   {id:53, date: new Date("2022-12-04"), registeredList:this.registeredList2}
-  // ];
 
   constructor(
     private registeredService: RegisteredService,
     private eventservice: EventService,
+    private translateService: TranslateService
 
   ) { }
 
-
-
-  ngOnInit(): void {
-    this.getRegistered();
-    this.getNextEvent(3);
+  async ngOnInit() {
+    this.registeredService.getRegisteredList().pipe(
+      switchMap(()=> {
+        return  this.registeredService.getRegisteredList()
+      }), 
+      tap((registeredList)=>{
+        this.getNextEvent(3, registeredList)
+      })
+    ).subscribe() 
   }
 
-  getNextEvent(limit:number) {
+  getNextEvent(limit:number, registeredList:any) {
     this.eventservice.getNextEvents({params:{limit: limit}}).subscribe((data) => {
       this.nextEvents = data;
-      console.log(this.nextEvents)
       this.nextEvents.forEach((element: any, index:number) => {
+        const currentList:any = registeredList.filter((registered: { id_event: any; }) => 
+          registered.id_event == element.id
+        )
         this.events[index] = {
           id: element.id,
           date: element.date,
           type: element.type,
-          registeredList: this.registeredList
-        } ;
-        console.log(element.id);       
+          registeredList: currentList
+        } ;      
       });
       console.log(this.events);
     })
   }
 
-  private getRegistered(): void {
-    this.registeredService.getEventAttendees().subscribe((registeredList) => {
-      this.registeredList = registeredList;
+  getRegistered() {
+    this.registeredService.getRegisteredList().subscribe((registeredList) => {
+      this.registeredList = registeredList
     });
   }
-
-  public DisplayList(){
-    console.log(this.registeredList)
-  }
   
+  public getColorByCountry(eventType:string): string {
+    if (eventType === 'jp') {
+      return "gengoffee-lightred-bg";
+    }
+    
+    return "gengoffee-lightblue-bg";
+  }
+
+  public getTranslatedDate(theDate:Date) {
+    const eventDate = new Date(theDate);
+    const translatedDay = this.translateService.instant('days.' + eventDate.getDay().toString());
+    const translatedMonth = this.translateService.instant('months.' + eventDate.getMonth().toString());
+    const dayNumber = eventDate.getDate();
+    const year = eventDate.getFullYear();
+
+    return this.translateService.instant('fullDate', {day: translatedDay, month: translatedMonth, dayNumber: dayNumber, year: year });
+  }
+
 }
--- src/app/services/registered.service.ts
+++ src/app/services/registered.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 
@@ -10,9 +11,9 @@ export class RegisteredService {
 
   constructor(private http: HttpClient) { }
   private url = environment.API_URL
-
-  public getEventAttendees(options?: any) { 
-    return this.http.get<Event>(this.url+"getEventAttendees", options); 
+  
+  public getRegisteredList(options?: any):Observable<any> {
+    return this.http.get<Event>(this.url+"getRegisteredList", options)
   }
 
 }
