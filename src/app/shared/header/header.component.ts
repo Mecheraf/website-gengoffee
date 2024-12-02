8ac780e392881eda83069d6b8128bba377dff6d3
Alan Mecheraf <mecheraf@gmail.com>
2024-12-02 16:27:24 +0900

    Added menu for admin

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,17 +1,30 @@
 import { ChangeDetectorRef, Component, HostListener, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AuthGuard } from 'src/app/auth/auth.guard';
+import { Router } from '@angular/router';
+import {Location,LocationStrategy,PathLocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [
+    Location,
+    { provide: LocationStrategy, useClass: PathLocationStrategy }
+  ]
 })
 export class HeaderComponent implements OnInit {
 
   public showMobileMenu: boolean = false;
   public isMobile: boolean = false;
+  public onAdmin:boolean = false;
 
-  constructor(public translate: TranslateService, private cdRef:ChangeDetectorRef) { }
+  constructor(
+    public translate: TranslateService, 
+    private cdRef:ChangeDetectorRef,
+    private router:Router,
+    private location:Location
+  ) { }
 
   ngOnInit(): void {
     this.getWindowSize();
@@ -33,5 +46,18 @@ export class HeaderComponent implements OnInit {
   @HostListener('window:resize', ['$event'])
   getWindowSize() {
     this.isMobile = window.innerWidth < 768;
+  }
+
+  onAdminPage():boolean{
+    console.log(this.router.url)
+    if(this.router.url === "/admin/events" || this.router.url === "/admin/registered"){
+      console.log("Hello")
+      return true
+    } 
+    return false
+  }
+
+  isAdvancedRoute(): boolean {
+    return this.location.path().indexOf('/admin/events') > -1;
   }
 }
